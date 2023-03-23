21e4857001e9b24595c61fdd837d2f43b06fe876
shreyas patil <shreyas@shreyas.local>
2023-03-24 01:37:20 +0530

    added details page for viewing single movie details

--- js/script.js
+++ js/script.js
@@ -100,6 +100,75 @@ function highLightActiveLink(){
     })
 }
 
+async function displayMovieDetails() {
+    const movieId = window.location.search.split('=')[1];
+  
+    const movie = await getApiData(`movie/${movieId}`);
+  
+    const div = document.createElement('div');
+  
+    div.innerHTML = `
+    <div class="details-top">
+    <div>
+    ${
+      movie.poster_path
+        ? `<img
+      src="https://image.tmdb.org/t/p/w500${movie.poster_path}"
+      class="card-img-top"
+      alt="${movie.title}"
+    />`
+        : `<img
+    src="../images/no-image.jpg"
+    class="card-img-top"
+    alt="${movie.title}"
+  />`
+    }
+    </div>
+    <div>
+      <h2>${movie.title}</h2>
+      <p>
+        <i class="fas fa-star text-primary"></i>
+        ${movie.vote_average.toFixed(1)} / 10
+      </p>
+      <p class="text-muted">Release Date: ${movie.release_date}</p>
+      <p>
+        ${movie.overview}
+      </p>
+      <h5>Genres</h5>
+      <ul class="list-group">
+        ${movie.genres.map((genre) => `<li>${genre.name}</li>`).join('')}
+      </ul>
+      <a href="${
+        movie.homepage
+      }" target="_blank" class="btn">Visit Movie Homepage</a>
+    </div>
+  </div>
+  <div class="details-bottom">
+    <h2>Movie Info</h2>
+    <ul>
+      <li><span class="text-secondary">Budget:</span> $${
+        movie.budget
+      }</li>
+      <li><span class="text-secondary">Revenue:</span> $${
+        movie.revenue
+      }</li>
+      <li><span class="text-secondary">Runtime:</span> ${
+        movie.runtime
+      } minutes</li>
+      <li><span class="text-secondary">Status:</span> ${movie.status}</li>
+    </ul>
+    <h4>Production Companies</h4>
+    <div class="list-group">
+      ${movie.production_companies
+        .map((company) => `<span>${company.name}</span>`)
+        .join(', ')}
+    </div>
+  </div>
+    `;
+  
+    document.querySelector('#movie-details').appendChild(div);
+  }
+  
 
 function init(){
     switch(global.currentPage){
@@ -112,13 +181,15 @@ function init(){
             console.log("shows")
             displayPopShows()
             break
-        case 'movie-details':
+        case '/movie-details.html':
+            
             console.log("movie-details")
+            displayMovieDetails()
             break
-        case 'search.html':
+        case '/search.html':
             console.log("search")
             break
-        case 'tv-details':
+        case '/tv-details.html':
             console.log("tv-details")
             break
 
